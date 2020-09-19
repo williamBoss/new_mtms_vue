48cf77f3749ad8bfee4157c079d62289ef9673ea
王舒宇 <549708589@qq.com>
2020-09-19 16:02:54 +0800

    sf36

---
+++ src/views/questionnaire/Sf36Questionnaire.vue
@@ -0,0 +1,15 @@
+<template>
+</template>
+<script>
+export default {
+  name: 'Sf36Questionnaire',
+  data () {
+    return {
+      layout: 'vertical',
+      eq5d5lForm: {}
+    }
+  }
+}
+</script>
+<style scoped>
+</style>
